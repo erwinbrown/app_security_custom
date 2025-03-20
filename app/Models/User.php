7af777da27c084070cc12d68de5d67174983efa7
erwinbrown <erwinbkfire@gmail.com>
2025-03-20 15:31:09 -0500

    002

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    // atributos no serializables
+    protected $hidden = [
+
+        'password',
+        'token'
+
+    ];
+        
+    
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         for ($i=0; $i <= 3 ; $i++) { 
+            User::create([
+              'username' => "usuario0$i",
+              'email' => "usuario0$i@gmail.com",
+              'password' => bcrypt('Z123pasS'),
+              'email_verified_at' => Carbon::now(),
+              'active' => true,
+
+            ]);
+         }
     }
 }
